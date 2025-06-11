f0396afe29a729a685fa2dfb2bedadf419f0b4ef
AKhmad-124 <akhmadsameh@gmail.com>
2025-06-11 22:06:18 +0300

    made the relace function

---
+++ problem 51/Problem51.m
@@ -0,0 +1,37 @@
+clc;clearvars;
+function answer = check_prime(number)
+i=2;
+while i <= round(sqrt(number))
+    if mod(number,i) == 0
+        answer = 0;
+        return 
+    end
+    i = i+2;
+end
+answer=1;
+end
+
+function amount = divide_and_replace(number,indexvec,max_tries)
+number_vec = num2str(number) -'0';
+y = zeros(size(number_vec))
+x = zeros(size(indexvec));
+for i = 0:9
+    
+    number_vec(indexvec) = x;
+    x = x+1;
+    new_number = str2double(sprintf('%d',number_vec))
+end
+
+end
+divide_and_replace(56993,[3,4],1)
+
+
+% x=num2str(56993-56003) -'0'
+% y= (56993-56113)
+%  56993-56333
+%  56993-56443
+%  56993-56663
+%  56993-56773
+%  56993-56993
+% x
+% b = str2double(sprintf('%d',x))
